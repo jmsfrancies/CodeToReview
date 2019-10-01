725cdd9155ebc3f8a2f9c8f502612400605764c5
JamesFrancies <jmsfrancies@yahoo.com>
2019-10-01 14:48:12 -0500

    Updates!

--- CodeForReview/CreatingTables.sql
+++ CodeForReview/CreatingTables.sql
@@ -1,14 +1,14 @@
 /*CREATE DATABASE FILE*/
 CREATE DATABASE
-MyGunStore
-GO
+MyGunStore;
+
 CREATE TABLE Firearms(
 FirearmID        INT          NOT NULL PRIMARY KEY,
 FirearmBrand     varchar(50)  NOT NULL,
 FirearmType      varchar(50)  NOT NULL,
 ActionType       varchar(50)  NOT NULL,
 Caliber          varchar(50)  NOT NULL,
-FirearmPrice     money        NOT NULL,
+FirearmPrice     Decimal(2)   NOT NULL,
 FirearmQuantity  INT          NULL
 );
 
@@ -16,7 +16,7 @@ CREATE TABLE Accessories(
 AccessoryID  INT NOT NULL PRIMARY KEY,
 TypeOfAccessory varchar(50) NOT NULL,
 AccessoryBrand  varchar(50) NOT NULL,
-AccessoryPrice  money       NOT NULL,
+AccessoryPrice  Decimal(2)    NOT NULL,
 Quantity        INT         NULL
 );
 
@@ -24,7 +24,7 @@ CREATE TABLE Knives(
 KnifeID       INT         NOT NULL PRIMARY KEY,
 KnifeBrand    varchar(50) NOT NULL,
 KnifeType     varchar(50) NOT NULL,
-KnifePrice    money       NOT NULL,
+KnifePrice    Decimal(2)  NOT NULL,
 KnifeQuantity INT         NULL
 );
 
@@ -36,7 +36,7 @@ TypeOfBulllet  varchar(50)  NOT NULL,
 BulletWeight   varchar(50)  NOT NULL,
 FeetPerSecond  INT          NOT NULL,
 FootLbs        INT          NOT NULL,
-AmmoPrice      money        NOT NULL,
+AmmoPrice      Decimal(2)   NOT NULL,
 AmmoQuantity   INT          NULL
 );
 
@@ -44,7 +44,7 @@ CREATE TABLE GunCleaning(
 CleaningID        INT         NOT NULL PRIMARY KEY,
 CleaningBrand     varchar(50) NOT NULL,
 TypeOfCleaner     varchar(50) NOT NULL,
-PriceOfCleaner    money       NOT NULL,
+PriceOfCleaner    Decimal(2)  NOT NULL,
 QuantityOfCleaner INT         NULL
 );
 
@@ -74,49 +74,49 @@ DiscountDescription varchar(128) NOT NULL
 
 ALTER TABLE Knives
 ADD KnifeModel varchar(50),
-BladeLength Decimal(5,2),
-SteelType varchar(50),
-Restrictions varchar(50);
+ADD BladeLength Decimal(5,2),
+ADD SteelType varchar(50),
+ADD Restrictions varchar(50);
 
 ALTER TABLE Ammo
 ADD PerBox INT;
 
 ALTER TABLE Reloading
-ADD ToolPrice money NOT NULL,
-ToolQuantity INT NOT NULL;
+ADD ToolPrice Decimal(2) NOT NULL,
+ADD ToolQuantity INT NOT NULL;
 
 ALTER TABLE Firearms
 ADD ModelName varchar(50), 
-BarrelLength INT, 
-ChokeType varchar(50) NULL, 
-UsedOrNew varchar(50),
-MagazineCapacity INT;
+ADD BarrelLength INT, 
+ADD ChokeType varchar(50) NULL, 
+ADD UsedOrNew varchar(50),
+ADD MagazineCapacity INT;
 
 ALTER TABLE Accessories
-ADD DiscountID INT NULL
-CONSTRAINT Accesories_fk_Discounts
+ADD DiscountID INT NULL,
+ADD CONSTRAINT Accesories_fk_Discounts
 FOREIGN KEY (DiscountID) REFERENCES Discounts(DiscountID);
 
 
 ALTER TABLE Ammo
-ADD DiscountID INT NULL
-CONSTRAINT Ammo_fk_Discounts
+ADD DiscountID INT NULL,
+ADD CONSTRAINT Ammo_fk_Discounts
 FOREIGN KEY (DiscountID) REFERENCES Discounts(DiscountID);
 
 
 ALTER TABLE Knives
-ADD DiscountID INT NULL
-CONSTRAINT Knives_fk_Discounts
+ADD DiscountID INT NULL,
+ADD CONSTRAINT Knives_fk_Discounts
 FOREIGN KEY (DiscountID) REFERENCES Discounts(DiscountID);
 
 
 ALTER TABLE Reloading
-ADD DiscountID INT NULL
-CONSTRAINT Reloading_fk_Discounts
+ADD DiscountID INT NULL,
+ADD CONSTRAINT Reloading_fk_Discounts
 FOREIGN KEY (DiscountID) REFERENCES Discounts(DiscountID);
 
 
 ALTER TABLE Firearms
-ADD DiscountID INT NULL
-CONSTRAINT Firearms_fk_Discounts
+ADD DiscountID INT NULL,
+ADD CONSTRAINT Firearms_fk_Discounts
 FOREIGN KEY (DiscountID) REFERENCES Discounts(DiscountID);
