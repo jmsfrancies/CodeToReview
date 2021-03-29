b09199642f6ca95da9d4563f11303e94025be7ce
James Francies <jfrancies1@live.maryville.edu>
2021-03-29 12:37:25 -0500

    added the email_tester function

--- PythonFiles/emailcheck.py
+++ PythonFiles/emailcheck.py
@@ -1,16 +1,21 @@
 import emailcheck as email
 
+def email_tester(email_to_test):
+    if ".com" in email_to_test:
+        print("Company Email ")
+    elif ".edu" in email_to_test:
+        print("Educational Email ")
+    elif ".net" in email_to_test:
+        print("Network Email ")
+    elif ".org" in email_to_test:
+        print("Organizational Email ")
+    else:
+        print("invalid Email Address")
+
 def main():
     email_to_test = str(input("please enter your email: "))
     if "@" in email_to_test:
-        if "gmail.com" in email_to_test:
-            print("Gmail! ")
-        elif "yahoo.com" in email_to_test:
-            print("Yahoo! ")
-        elif "charter.net" in email_to_test:
-            print("Charter Network")
-        else:
-            print("invalid Email Address")
+        email_tester(email_to_test)
     else:
         print("No At sign")
 
